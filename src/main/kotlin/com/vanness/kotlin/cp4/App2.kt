48805bbf0eb758bf3a7b0bd11470a78e9f706874
Vanness28 <vannesstumampas28@gmail.com>
2023-04-13 09:39:25 +0800

    Cp-4 Commit

--- src/main/kotlin/com/vanness/kotlin/cp4/App2.kt
+++ src/main/kotlin/com/vanness/kotlin/cp4/App2.kt
@@ -1,13 +1,17 @@
 package com.vanness.kotlin.cp4
 
+import kotlin.math.pow
+
 fun main() {
     println("Masukkan Jumlah Case : ")
     val t = readLine()!!.toInt()
+    val result = StringBuilder()
     println("Masukkan Angka : ")
     repeat(t) {
         val n = readLine()!!
         val b1 = n.toInt().toString(2).count { it == '1' }
         val b2 = n.toInt(16).toString(2).count { it == '1' }
-        println("$b1 $b2")
+        result.append("$b1 $b2\n")
     }
+    println(result.toString())
 }
